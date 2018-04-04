05452b2ca591581d170964437e1e41e1af4988f5
TrentPearson <trent.pearson172@hotmail.com>
2018-04-04 15:19:09 +1000

    new activities have been added

---
+++ app/src/main/java/com/example/trent/assignment1/Adventure3.java
@@ -0,0 +1,13 @@
+package com.example.trent.assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Adventure3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adventure3);
+    }
+}
