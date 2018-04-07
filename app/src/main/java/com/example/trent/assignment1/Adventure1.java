63b76c6b754ce792aa862eb70ed5283c98f409ec
TrentPearson <trent.pearson172@hotmail.com>
2018-04-07 23:21:43 +1000

    additional functions

--- app/src/main/java/com/example/trent/assignment1/Adventure1.java
+++ app/src/main/java/com/example/trent/assignment1/Adventure1.java
@@ -4,17 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class Adventure1 extends AppCompatActivity {
+public class Adventure1 extends AppCompatActivity
+{
+    TextView hiddenView;
+    TextView attackPhrase;
+    Button showButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adventure1);
-    }
     public void sendBack(View view)
     {
         Intent intent = new Intent (this, Adventure.class);
         startActivity(intent);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adventure1);
+
+        showButton = findViewById(R.id.btnAttack1);
+        showButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                hiddenView = findViewById(R.id.attackPhrase);
+                attackPhrase.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
 }
