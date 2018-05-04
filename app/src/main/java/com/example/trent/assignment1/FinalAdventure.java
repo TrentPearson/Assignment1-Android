b384447f63458d3140d49f0678ca4ecfebfc94d2
TrentPearson <trent.pearson172@hotmail.com>
2018-05-04 15:00:41 +1000

    Final screen in creation process.

--- app/src/main/java/com/example/trent/assignment1/FinalAdventure.java
+++ app/src/main/java/com/example/trent/assignment1/FinalAdventure.java
@@ -11,12 +11,20 @@ import android.widget.TextView;
 public class FinalAdventure extends AppCompatActivity
 {
 
+    TextView t1, t2, t3;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_adventure);
+
+        t1 = findViewById(R.id.motherMessage1);
+        t2 = findViewById(R.id.motherMessage2);
+        t3 = findViewById(R.id.motherMessage3);
+
+
 
 
     }
